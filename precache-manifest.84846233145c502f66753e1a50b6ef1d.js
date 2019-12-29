9364699e2b7fd624da1b8113f32c74b3691d5439
unknown <vetka921@gmail.com>
2019-12-29 21:59:42 +0700

    Updates

---
+++ precache-manifest.84846233145c502f66753e1a50b6ef1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e072d0b98dd192ca3bd15c7e1aa26758",
+    "url": "/bubbly-page/index.html"
+  },
+  {
+    "revision": "73e1cdb4473c73954381",
+    "url": "/bubbly-page/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "ee54d93a5472afe03af2",
+    "url": "/bubbly-page/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "73e1cdb4473c73954381",
+    "url": "/bubbly-page/static/js/2.00651d56.chunk.js"
+  },
+  {
+    "revision": "ee54d93a5472afe03af2",
+    "url": "/bubbly-page/static/js/main.1b6a2714.chunk.js"
+  },
+  {
+    "revision": "aa629ee165da5d99daf7",
+    "url": "/bubbly-page/static/js/runtime-main.1554a7bf.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/bubbly-page/static/media/persik.4e1ec840.png"
+  }
+]);
