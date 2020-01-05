2ace1e9daec7a36bb896ac1e3e3af12fb35ebef8
unknown <vetka921@gmail.com>
2020-01-06 02:14:55 +0700

    Updates

---
+++ precache-manifest.918a3c67149ce549f7a38a76c0d16706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9796409a3855a17d26480a2c00f3a4e",
+    "url": "/bubbly-page/index.html"
+  },
+  {
+    "revision": "1ab268ae8a7d7d05c8c6",
+    "url": "/bubbly-page/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "885d2391c05390b1e5f4",
+    "url": "/bubbly-page/static/css/main.0a8e021c.chunk.css"
+  },
+  {
+    "revision": "1ab268ae8a7d7d05c8c6",
+    "url": "/bubbly-page/static/js/2.72e21673.chunk.js"
+  },
+  {
+    "revision": "885d2391c05390b1e5f4",
+    "url": "/bubbly-page/static/js/main.6c780bb5.chunk.js"
+  },
+  {
+    "revision": "aa629ee165da5d99daf7",
+    "url": "/bubbly-page/static/js/runtime-main.1554a7bf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bubbly-page/precache-manifest.81829dd0ca22ef64bab67e8ef687b35e.js"
+  "/bubbly-page/precache-manifest.918a3c67149ce549f7a38a76c0d16706.js"
 );
 
 self.addEventListener('message', (event) => {
