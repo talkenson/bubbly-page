02ba2d5750278d564bc6dab4fd359ff04f94c7e6
unknown <vetka921@gmail.com>
2020-01-07 01:46:24 +0700

    Updates

---
+++ precache-manifest.b001efd290f33d7f25ebe0890de9a272.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfcf7833598bc5ce808be624172181bb",
+    "url": "/bubbly-page/index.html"
+  },
+  {
+    "revision": "36ca0db6f19410d7d686",
+    "url": "/bubbly-page/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "1e6b575722126dd42fea",
+    "url": "/bubbly-page/static/css/main.ce94963e.chunk.css"
+  },
+  {
+    "revision": "36ca0db6f19410d7d686",
+    "url": "/bubbly-page/static/js/2.cb76fc6a.chunk.js"
+  },
+  {
+    "revision": "1e6b575722126dd42fea",
+    "url": "/bubbly-page/static/js/main.4d82ef8c.chunk.js"
+  },
+  {
+    "revision": "aa629ee165da5d99daf7",
+    "url": "/bubbly-page/static/js/runtime-main.1554a7bf.js"
+  }
+]);
